31d13baee13a09c221127f1933b1868479d9b694
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2021-02-25 14:47:01 -0500

    password

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 packer build  \
-    -var 'aws_access_key=AKIAQ7IKVIAKMD6OCMWJ' \
-    -var 'aws_secret_key=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' \
     -var 'aws_region=us-east-1'  \
     -var 'subnet_id=subnet-e754e6c6'  \
     -var 'ami_users=359410113455' \
