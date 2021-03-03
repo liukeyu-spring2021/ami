241defeb8507a0a64aae7f8819fce46113239fbc
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2021-03-03 11:53:46 -0500

    codedeploy and ruby

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ packer build  \
     -var 'aws_region=us-east-1'  \
     -var 'subnet_id=subnet-e754e6c6'  \
     -var 'ami_users=359410113455' \
-    -var 'source_ami=ami-0817d428a6fb68645'  \
+    -var 'source_ami=ami-03d315ad33b9d49c4'  \
     ami.json
