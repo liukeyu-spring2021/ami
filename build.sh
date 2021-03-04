abe1279ce075e56bab1c3ddff5c7d629598df0c2
Keyu Liu <keyuliu@KeyudeMacBook-Pro.local>
2021-03-03 19:34:49 -0500

    codedeploy and ruby

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 packer build  \
+    -var 'aws_access_key=REDACTED' \
+    -var 'aws_secret_key=REDACTED' \
     -var 'aws_region=us-east-1'  \
     -var 'subnet_id=subnet-e754e6c6'  \
     -var 'ami_users=359410113455' \
